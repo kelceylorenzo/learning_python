6a533906aa07327133054c074d23de1b6b3e29bc
Kelcey Lorenzo <kelceylorenzo.dev@gmail.com>
2018-06-05 11:29:27 -0700

    Delete own_list.py

--- learning_python/chapter3/own_list.py
+++
@@ -1,4 +0,0 @@
-transportation = ["train", "airplane", "go-kart"]
-print("I want to ride the " + transportation[0] + " to San Diego.")
-print("To get home to Hawaii, I take the " + transportation[1])
-print("Riding " + transportation[2] + " is fun, but really loud.")
