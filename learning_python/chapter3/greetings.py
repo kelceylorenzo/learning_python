b1a95f2f9dd62daf4a1edd0279bd1f2f6a5fb0a0
Kelcey Lorenzo <kelceylorenzo.dev@gmail.com>
2018-06-05 11:29:15 -0700

    Delete greetings.py

--- learning_python/chapter3/greetings.py
+++
@@ -1,6 +0,0 @@
-names = ['ben', 'gary', 'jon', 'alexa']
-message = "Hello, "
-print(message + names[0].title() + "!")
-print(message + names[1].title() + "!")
-print(message + names[2].title() + "!")
-print(message + names[3].title() + "!")
