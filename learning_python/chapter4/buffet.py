279c572def1e989f97bbf95e7675116cbedb994e
Kelcey Lorenzo <kelceylorenzo.dev@gmail.com>
2018-06-05 11:29:50 -0700

    Delete buffet.py

--- learning_python/chapter4/buffet.py
+++
@@ -1,8 +0,0 @@
-basic_foods = ('hamburgers', 'pizza', 'sandwiches', 'soup', 'french fries')
-for food in basic_foods:
-    print(food)
-
-print('\n')
-basic_foods = ('hamburgers', 'sushi', 'sandwiches', 'churros', 'french fries')
-for food in basic_foods:
-    print(food)
